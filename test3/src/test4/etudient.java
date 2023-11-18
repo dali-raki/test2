8debc0210d6b92292f08aa584b5253aab1f480b7
daliraki <daliraki1@gmail.com>
2023-11-18 11:57:36 +0100

    commit 3

--- test3/src/test4/etudient.java
+++ test3/src/test4/etudient.java
@@ -1,5 +1,10 @@
 package test4;
 
 public class etudient {
+	private int x;
 
+	public etudient(int z) {
+		
+		this.x=z;
+	}
 }
--- test3/src/test4/main2.java
+++ test3/src/test4/main2.java
@@ -4,6 +4,10 @@ public class main2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		etudient s=new etudient(5);
+		
+		System.out.print(s);
 
 	}
 
